e129139690a76a6a9009ca423658c18eb4a4ef16
IHeb <ihebferjani04@gmail.com>
2021-12-04 17:53:17 +0100

    Fix basePath

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,4 +1,4 @@
 export const GlobalVariable = Object.freeze({
     IMAGE_BASE_PATH: 'https://djotech-backend-test.herokuapp.com/uploads/',
-    BASE_PATH: 'http://localhost:3000',
+    BASE_PATH: 'http://djotech-backend-test.herokuapp.com',
 });
