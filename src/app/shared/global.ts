bc2ac012bf18b48131ae60a8b8b6602efed00274
IHeb <ihebferjani04@gmail.com>
2021-12-06 20:06:09 +0100

    update

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,5 +1,5 @@
 export const GlobalVariable = Object.freeze({
     IMAGE_BASE_PATH: 'https://djotech-backend-test.herokuapp.com/uploads/',
-    BASE_PATH: 'http://localhost:3000',
+    BASE_PATH: 'http://djotech-backend-test.herokuapp.com',
     STATES: ["Neuf", "Bon occasion", "Occasion"]
 });
