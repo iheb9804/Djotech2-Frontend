b18e47a91f4637949784084607e99d26f7734e6b
IHeb <ihebferjani04@gmail.com>
2022-03-09 16:10:34 +0100

    Update Test

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,6 +1,6 @@
 export const GlobalVariable = Object.freeze({
-    IMAGE_BASE_PATH: 'https://djotech-backend-test.herokuapp.com/uploads/',
-    BASE_PATH: 'https://djotech-backend-test.herokuapp.com',
+    IMAGE_BASE_PATH: 'https://inventorymanagement-backend.herokuapp.com/uploads/',
+    BASE_PATH: 'https://inventorymanagement-backend.herokuapp.com/',
     STATES: ["Neuf", "Bon occasion", "Occasion"],
     MONTHES : ['Janvier', 'Février', 'Mars', 'Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
 });
