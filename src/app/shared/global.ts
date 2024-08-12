d3c634f0258b6ba52b4023363ee1e0e556d7823e
iheb9804 <ihebferjani0100@gmail.com>
2024-08-12 13:18:03 +0100

    Update global.ts

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,6 +1,6 @@
 export const GlobalVariable = Object.freeze({
     IMAGE_BASE_PATH: 'https://djotech-backend-test.herokuapp.com/uploads/',
-    BASE_PATH: 'https://djotech-backend-ubgs.onrender.com',
+    BASE_PATH: 'https://djotech2-backend.onrender.com',
     STATES: ["Neuf", "Bon occasion", "Occasion"],
     MONTHES : ['Janvier', 'Février', 'Mars', 'Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
 });
